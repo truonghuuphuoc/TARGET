851c516c9ca73506efd1a63a113e787f1dffd9ce
Henry Truong <henry.truong@styl.solutions>
2017-11-13 16:35:28 +0700

    - Add function to control 4 leds

---
+++ TARGET/project/stm32f10x_stdperiph_template/USER/phnLed.c
@@ -0,0 +1,100 @@
+#include "phnLed.h"
+
+
+#include <stdio.h>
+#include <stm32f10x.h>
+
+
+void phnLed_Init()
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	
+	/* Enable the Clock */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_AFIO, ENABLE);
+	
+	/**
+	 *	Pin B.11 
+	 */
+	
+	/* Configure pin as output pull-up */
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	
+	/**
+	 *	Pin B.12 
+	 */
+	
+	/* Configure pin as output pull-up */
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	
+	/**
+	 *	Pin B.13 
+	 */
+	
+	/* Configure pin as output pull-up */
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	
+	/**
+	 *	Pin B.14 
+	 */
+	
+	/* Configure pin as output pull-up */
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	
+	GPIO_ResetBits(GPIOB, GPIO_Pin_11);
+	GPIO_ResetBits(GPIOB, GPIO_Pin_12);
+	GPIO_ResetBits(GPIOB, GPIO_Pin_13);
+	GPIO_ResetBits(GPIOB, GPIO_Pin_14);
+}
+
+
+void phnLed_SetDeviceLeds(uint8_t device)
+{
+	switch(device)
+	{
+		case LED_DEV_MASSTER:
+			GPIO_SetBits(GPIOB, GPIO_Pin_11);
+			break;
+		
+		case LED_DEV_SLAVE_1:
+			GPIO_SetBits(GPIOB, GPIO_Pin_13);
+			break;
+		
+		case LED_DEV_SLAVE_2:
+			GPIO_SetBits(GPIOB, GPIO_Pin_12);
+			break;
+		
+		case LED_DEV_SLAVE_3:
+			GPIO_SetBits(GPIOB, GPIO_Pin_11);
+			GPIO_SetBits(GPIOB, GPIO_Pin_12);
+			break;
+		
+		default:
+			break;
+	}
+}
+
+
+void phnLed_SetLedStatus()
+{
+	GPIO_SetBits(GPIOB, GPIO_Pin_14);
+}
+
+
+void phnLed_ClearLedStatus()
+{
+	GPIO_ResetBits(GPIOB, GPIO_Pin_14);
+}
+
+
---
+++ TARGET/project/stm32f10x_stdperiph_template/USER/phnLed.h
@@ -0,0 +1,59 @@
+#ifndef __PHN_LED_H
+#define __PHN_LED_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+
+#include <stdint.h>	 	 
+
+#define LED_DEV_MASSTER		0x00
+#define LED_DEV_SLAVE_1		0x01			 
+#define LED_DEV_SLAVE_2		0x02
+#define LED_DEV_SLAVE_3		0x03
+	 
+/**
+  * @brief  
+  * @param  None
+  * @retval None
+  */
+void phnLed_Init(void);
+
+
+/**
+  * @brief  Set led to indicate device with 3 leds
+  * @param  
+  * @retval None
+  */
+void phnLed_SetDeviceLeds(uint8_t device);
+
+/**
+  * @brief  Turn on LED status
+  * @param  None
+  * @retval None
+  */
+void phnLed_SetLedStatus(void);
+
+/**
+  * @brief  Turn off LED status
+  * @param  None
+  * @retval None
+  */
+void phnLed_ClearLedStatus(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*__PHN_LED_H */
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
